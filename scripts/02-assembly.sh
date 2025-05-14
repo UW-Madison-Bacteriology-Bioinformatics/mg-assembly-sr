fac95062bf5eb47c8dc936ecb14750f1a506de03
Patricia Tran <ptran5@wisc.edu>
2025-05-14 11:51:35 -0500

    use memory and metagenomic option.

--- scripts/02-assembly.sh
+++ scripts/02-assembly.sh
@@ -2,16 +2,20 @@
 
 SAMPLE="$1"
 CPU="$2"
+MEMORY="$3"
+echo $MEMORY
+MEMORY="${MEMORY%GB*}"
+echo $MEMORY
 
 ls -lh
 
 spades.py -1 ${SAMPLE}_out.R1.fastq.gz \
 	-2 ${SAMPLE}_out.R2.fastq.gz \
 	-o ${SAMPLE}_spades_output \
-	--isolate \
+	--meta \
 	--only-assembler \
 	-t ${CPU} \
-	-m 600
+	-m ${MEMORY}
 
 ls -lh
 
